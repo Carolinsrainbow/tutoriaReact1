595a8bcfea42796769d1a18d0a445f17dddbe0a9
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2022-07-11 17:00:45 -0400

    Update menu.jsx
    Se incorporan partes del navbar

--- pag/src/Componentes/menu.jsx
+++ pag/src/Componentes/menu.jsx
@@ -9,21 +9,27 @@ class Menu extends React.Component {
  
   		<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-warning">
  
-		    <a className="navbar-brand" href="#">Retro Consultancy</a>
+		    <a className="navbar-brand">Retro Consultancy</a>
 		    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 		     	<span className="navbar-toggler-icon"></span>
 		    </button>
 		    <div className="collapse navbar-collapse" id="navbarCollapse">
-			    <ul className="navbar-nav mr-auto">
+			    <ul className="navbar-nav">
 			        
 			        <li className="nav-item">
-			          	<a className="nav-link" href="#">Nosotros</a>
+			          	<a className="nav-link">Home</a>
 			        </li>
 			        <li className="nav-item">
-			          	<a className="nav-link" href="#">Servicios</a>
+			          	<a className="nav-link">Services</a>
 			        </li>
 			        <li className="nav-item">
-			          	<a className="nav-link" href="#">Contacto</a>
+			          	<a className="nav-link">Portafolio</a>
+			        </li>
+                    <li className="nav-item">
+			          	<a className="nav-link">Pages</a>
+			        </li>
+                    <li className="nav-item">
+			          	<a className="nav-link">Contact</a>
 			        </li>
 			    </ul>
 			  
